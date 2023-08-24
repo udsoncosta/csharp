ad3bfbbe3aef73c6111eedc4508499441f487996
Udson Costa <141694537+udsoncosta@users.noreply.github.com>
2023-08-24 12:52:35 -0300

    Update Program.cs

--- Aula02/atividade01/Program.cs
+++ Aula02/atividade01/Program.cs
@@ -4,25 +4,15 @@
     {
         static void Main(string[] args)
         {
-            float nota1, nota2, nota3, nota4;
+            float salario, abono;
+ Console.WriteLine("Digite o salário: ");
+ salario = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Digite a primeira nota: ");
-            nota1 = Convert.ToSingle(Console.ReadLine());
+ Console.WriteLine("Digite o abono: ");
+ abono = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Digite a segunda nota: ");
-            nota2 = Convert.ToSingle(Console.ReadLine());
-
-            Console.WriteLine("Digite a terceira nota: ");
-            nota3 = Convert.ToSingle(Console.ReadLine());
-
-            Console.WriteLine("Digite a quarta nota: ");
-            nota4 = Convert.ToSingle(Console.ReadLine());
-
-
-            Console.WriteLine($"{nota1} + {nota2} + {nota3} + {nota4} / 4 = " + ((nota1 + nota2 + nota3 + nota4) / 4));
-            Console.WriteLine("A sua média é de " + Math.Round((nota1 + nota2 + nota3 + nota4) / 4, 1));
-
-
+ Console.WriteLine($"{salario} + {abono} = " + (salario + abono));
+ Console.WriteLine("O seu salario somado ao abono é " + (salario + abono));
         }
     }
 }
