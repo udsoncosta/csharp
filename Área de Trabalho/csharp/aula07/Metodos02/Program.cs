97682ceaf437aeee727160845a3137959c5c4447
udsoncosta <udsoncostasantana@gmail.com>
2023-08-30 22:02:41 -0300

    aula07

---
+++ Área de Trabalho/csharp/aula07/Metodos02/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Metodos02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MetodoInternal();
+            MetodoPublico();
+            MetodoProtegido();
+            MetodoPrivado();
+        }
+
+        public static void MetodoPublico()
+        {
+            Console.WriteLine("Método público - Classe program");
+        }
+        internal static void MetodoInternal()
+        {
+            Console.WriteLine("Método Internal - Classe program");
+        }
+        protected static void MetodoProtegido()
+        {
+            Console.WriteLine("Método protegido - Classe program");
+        }
+        private static void MetodoPrivado()
+        {
+            Console.WriteLine("Método privado - Classe program");
+        }
+    }
+}
