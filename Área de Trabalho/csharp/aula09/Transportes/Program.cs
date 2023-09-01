6fff2f881c22a1f2fe3a558abbe7edadecb92079
udsoncosta <udsoncostasantana@gmail.com>
2023-09-01 10:29:22 -0300

    aula09

---
+++ Área de Trabalho/csharp/aula09/Transportes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using System.Runtime.ConstrainedExecution;
+
+namespace Transportes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transporte t1 = new Transporte(5);
+
+            t1.Visualizar();
+
+            Console.WriteLine();
+
+            Terrestre t2 = new Terrestre(4, 4, 100);
+            t2.Visualizar();
+
+            Automovel a1 = new Automovel(10, 10, 280, "azul", 1, "GGG-5555", 7);
+
+            a1.Visualizar();
+        }
+    }
+}
