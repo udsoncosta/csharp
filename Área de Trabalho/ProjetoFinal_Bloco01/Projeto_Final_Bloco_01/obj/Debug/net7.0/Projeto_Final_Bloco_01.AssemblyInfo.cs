fc6e5f02e9add1cd4c63ebf363080e46e613a5ad
udsoncosta <udsoncostasantana@gmail.com>
2023-09-11 14:51:24 -0300

    projeto_final_bloco_01

--- Área de Trabalho/ProjetoFinal_Bloco01/Projeto_Final_Bloco_01/obj/Debug/net7.0/Projeto_Final_Bloco_01.AssemblyInfo.cs
+++ Área de Trabalho/ProjetoFinal_Bloco01/Projeto_Final_Bloco_01/obj/Debug/net7.0/Projeto_Final_Bloco_01.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Final_Bloco_01")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("projeto_final_bloco_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Projeto_Final_Bloco_01")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Final_Bloco_01")]
+[assembly: System.Reflection.AssemblyProductAttribute("projeto_final_bloco_01")]
+[assembly: System.Reflection.AssemblyTitleAttribute("projeto_final_bloco_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
